5f322d32ca5501e252f4be819b76e8fa953b9eb0
diogo464 <diogo464@protonmail.com>
2023-01-25 16:13:32 +0000

    opencensus to otel metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,110 +1,212 @@
 package metrics
 
 import (
+	"context"
+	"time"
+
+	"github.com/diogo464/telemetry"
 	pb "github.com/libp2p/go-libp2p-kad-dht/pb"
-	"go.opencensus.io/stats"
-	"go.opencensus.io/stats/view"
-	"go.opencensus.io/tag"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/metric/instrument/syncfloat64"
+	"go.opentelemetry.io/otel/metric/instrument/syncint64"
+	"go.opentelemetry.io/otel/metric/unit"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
 )
+
+// TODO: InstanceID should be added to Metrics.Attributes
 
 var (
-	defaultBytesDistribution        = view.Distribution(1024, 2048, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824, 4294967296)
-	defaultMillisecondsDistribution = view.Distribution(0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 5000, 10000, 20000, 50000, 100000)
+	Scope = instrumentation.Scope{
+		Name:    "libp2p.io/dht/kad",
+		Version: "0.18.0",
+	}
+
+	KeyMessageType = attribute.Key("message_type")
+	KeyPeerID      = attribute.Key("peer_id")
+	KeyInstanceID  = attribute.Key("instance_id")
+
+	AttributeMessagePutValue     = KeyMessageType.String("PutValue")
+	AttributeMessageGetValue     = KeyMessageType.String("GetValue")
+	AttributeMessageAddProvider  = KeyMessageType.String("AddProvider")
+	AttributeMessageGetProviders = KeyMessageType.String("GetProviders")
+	AttributeMessageFindNode     = KeyMessageType.String("FindNode")
+	AttributeMessagePing         = KeyMessageType.String("Ping")
+	AttributeMessageUnknown      = KeyMessageType.String("Unknown")
 )
 
-// Keys
-var (
-	KeyMessageType, _ = tag.NewKey("message_type")
-	KeyPeerID, _      = tag.NewKey("peer_id")
-	// KeyInstanceID identifies a dht instance by the pointer address.
-	// Useful for differentiating between different dhts that have the same peer id.
-	KeyInstanceID, _ = tag.NewKey("instance_id")
-)
-
-// UpsertMessageType is a convenience upserts the message type
-// of a pb.Message into the KeyMessageType.
-func UpsertMessageType(m *pb.Message) tag.Mutator {
-	return tag.Upsert(KeyMessageType, m.Type.String())
+type handlerEvent struct {
+	MessageType string `json:"message_type"`
+	Write       uint64 `json:"write"`
+	Handler     uint64 `json:"handler"`
 }
 
-// Measures
-var (
-	ReceivedMessages       = stats.Int64("libp2p.io/dht/kad/received_messages", "Total number of messages received per RPC", stats.UnitDimensionless)
-	ReceivedMessageErrors  = stats.Int64("libp2p.io/dht/kad/received_message_errors", "Total number of errors for messages received per RPC", stats.UnitDimensionless)
-	ReceivedBytes          = stats.Int64("libp2p.io/dht/kad/received_bytes", "Total received bytes per RPC", stats.UnitBytes)
-	InboundRequestLatency  = stats.Float64("libp2p.io/dht/kad/inbound_request_latency", "Latency per RPC", stats.UnitMilliseconds)
-	OutboundRequestLatency = stats.Float64("libp2p.io/dht/kad/outbound_request_latency", "Latency per RPC", stats.UnitMilliseconds)
-	SentMessages           = stats.Int64("libp2p.io/dht/kad/sent_messages", "Total number of messages sent per RPC", stats.UnitDimensionless)
-	SentMessageErrors      = stats.Int64("libp2p.io/dht/kad/sent_message_errors", "Total number of errors for messages sent per RPC", stats.UnitDimensionless)
-	SentRequests           = stats.Int64("libp2p.io/dht/kad/sent_requests", "Total number of requests sent per RPC", stats.UnitDimensionless)
-	SentRequestErrors      = stats.Int64("libp2p.io/dht/kad/sent_request_errors", "Total number of errors for requests sent per RPC", stats.UnitDimensionless)
-	SentBytes              = stats.Int64("libp2p.io/dht/kad/sent_bytes", "Total sent bytes per RPC", stats.UnitBytes)
-)
+type Metrics struct {
+	ReceivedMessages       syncint64.Counter
+	ReceivedMessageErrors  syncint64.Counter
+	ReceivedBytes          syncint64.Counter
+	InboundRequestLatency  syncfloat64.Histogram
+	OutboundRequestLatency syncfloat64.Histogram
+	SentMessages           syncint64.Counter
+	SentMessageErrors      syncint64.Counter
+	SentRequests           syncint64.Counter
+	SentRequestErrors      syncint64.Counter
+	SentBytes              syncint64.Counter
 
-// Views
-var (
-	ReceivedMessagesView = &view.View{
-		Measure:     ReceivedMessages,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	ReceivedMessageErrorsView = &view.View{
-		Measure:     ReceivedMessageErrors,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	ReceivedBytesView = &view.View{
-		Measure:     ReceivedBytes,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: defaultBytesDistribution,
-	}
-	InboundRequestLatencyView = &view.View{
-		Measure:     InboundRequestLatency,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: defaultMillisecondsDistribution,
-	}
-	OutboundRequestLatencyView = &view.View{
-		Measure:     OutboundRequestLatency,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: defaultMillisecondsDistribution,
-	}
-	SentMessagesView = &view.View{
-		Measure:     SentMessages,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	SentMessageErrorsView = &view.View{
-		Measure:     SentMessageErrors,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	SentRequestsView = &view.View{
-		Measure:     SentRequests,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	SentRequestErrorsView = &view.View{
-		Measure:     SentRequestErrors,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: view.Count(),
-	}
-	SentBytesView = &view.View{
-		Measure:     SentBytes,
-		TagKeys:     []tag.Key{KeyMessageType, KeyPeerID, KeyInstanceID},
-		Aggregation: defaultBytesDistribution,
-	}
-)
+	attributes   []attribute.KeyValue
+	eventHandler telemetry.EventEmitter
+}
 
-// DefaultViews with all views in it.
-var DefaultViews = []*view.View{
-	ReceivedMessagesView,
-	ReceivedMessageErrorsView,
-	ReceivedBytesView,
-	InboundRequestLatencyView,
-	OutboundRequestLatencyView,
-	SentMessagesView,
-	SentMessageErrorsView,
-	SentRequestsView,
-	SentRequestErrorsView,
-	SentBytesView,
+func New(provider metric.MeterProvider) (*Metrics, error) {
+	m := provider.Meter(Scope.Name, metric.WithInstrumentationVersion(Scope.Version), metric.WithSchemaURL(Scope.SchemaURL))
+
+	ReceivedMessages, err := m.SyncInt64().Counter(
+		"received_messages",
+		instrument.WithDescription("Total number of messages received per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	ReceivedMessageErrors, err := m.SyncInt64().Counter(
+		"received_message_errors",
+		instrument.WithDescription("Total number of errors for messages received per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	ReceivedBytes, err := m.SyncInt64().Counter(
+		"received_bytes",
+		instrument.WithDescription("Total received bytes per RPC"),
+		instrument.WithUnit(unit.Bytes),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	InboundRequestLatency, err := m.SyncFloat64().Histogram(
+		"inbound_request_latency",
+		instrument.WithDescription("Latency per RPC"),
+		instrument.WithUnit(unit.Milliseconds),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	OutboundRequestLatency, err := m.SyncFloat64().Histogram(
+		"outbound_request_latency",
+		instrument.WithDescription("Latency per RPC"),
+		instrument.WithUnit(unit.Milliseconds),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	SentMessages, err := m.SyncInt64().Counter(
+		"sent_messages",
+		instrument.WithDescription("Total number of messages sent per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	SentMessageErrors, err := m.SyncInt64().Counter(
+		"sent_message_errors",
+		instrument.WithDescription("Total number of errors for messages sent per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	SentRequests, err := m.SyncInt64().Counter(
+		"sent_requests",
+		instrument.WithDescription("Total number of requests sent per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	SentRequestErrors, err := m.SyncInt64().Counter(
+		"sent_request_errors",
+		instrument.WithDescription("Total number of errors for requests sent per RPC"),
+		instrument.WithUnit(unit.Dimensionless),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	SentBytes, err := m.SyncInt64().Counter(
+		"sent_bytes",
+		instrument.WithDescription("Total sent bytes per RPC"),
+		instrument.WithUnit(unit.Bytes),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tprovider := telemetry.DowncastMeterProvider(provider)
+	m2 := tprovider.TelemetryMeter("libp2p.io/telemetry")
+
+	eventHandler := m2.Event(
+		"kad.handler_timings",
+		instrument.WithDescription("Message handler timings"),
+	)
+
+	return &Metrics{
+		ReceivedMessages:       ReceivedMessages,
+		ReceivedMessageErrors:  ReceivedMessageErrors,
+		ReceivedBytes:          ReceivedBytes,
+		InboundRequestLatency:  InboundRequestLatency,
+		OutboundRequestLatency: OutboundRequestLatency,
+		SentMessages:           SentMessages,
+		SentMessageErrors:      SentMessageErrors,
+		SentRequests:           SentRequests,
+		SentRequestErrors:      SentRequestErrors,
+		SentBytes:              SentBytes,
+		eventHandler:           eventHandler,
+	}, nil
+}
+
+func (m *Metrics) Attributes(attrs ...attribute.KeyValue) []attribute.KeyValue {
+	if len(m.attributes) == 0 {
+		return attrs
+	}
+	joined := make([]attribute.KeyValue, 0, len(m.attributes)+len(attrs))
+	joined = append(joined, m.attributes...)
+	joined = append(joined, attrs...)
+	return joined
+}
+
+func (m *Metrics) Handler(ctx context.Context, t pb.Message_MessageType, write time.Duration, handler time.Duration) {
+	m.eventHandler.Emit(&handlerEvent{
+		MessageType: t.String(),
+		Write:       uint64(write.Nanoseconds()),
+		Handler:     uint64(handler.Nanoseconds()),
+	})
+}
+
+func MessageTypeAttribute(mt pb.Message_MessageType) attribute.KeyValue {
+	switch mt {
+	case pb.Message_PUT_VALUE:
+		return AttributeMessagePutValue
+	case pb.Message_GET_VALUE:
+		return AttributeMessageGetValue
+	case pb.Message_ADD_PROVIDER:
+		return AttributeMessageAddProvider
+	case pb.Message_GET_PROVIDERS:
+		return AttributeMessageGetProviders
+	case pb.Message_FIND_NODE:
+		return AttributeMessageFindNode
+	case pb.Message_PING:
+		return AttributeMessagePing
+	default:
+		return AttributeMessageUnknown
+	}
 }
